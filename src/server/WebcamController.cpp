3daee8bdad4f709e873514072d5c4de168fcb1c6
sabertoaster <phidaotram123@gmail.com>
2024-11-06 09:41:43 +0700

    stash: multithread camera

--- src/server/WebcamController.cpp
+++ src/server/WebcamController.cpp
@@ -71,15 +71,6 @@ HRESULT WebcamController::EnumerateDevices() {
     return hr;
 }
 
-// void SafeRelease(T **ppT)
-// {
-//     if (*ppT)
-//     {
-//         (*ppT)->Release();
-//         *ppT = NULL;
-//     }
-// }
-
 void StartWebcam_1() {
     int DeviceNumber = 0;
 
@@ -180,6 +171,88 @@ void StartWebcam_1() {
         /*Handle Error*/
     }
 
+    if (SUCCEEDED(hr)) {
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+
+        hr = CoCreateInstance(CLSID_CaptureGraphBuilder2, NULL, CLSCTX_INPROC_SERVER, IID_ICaptureGraphBuilder2,
+                              (void **) &pBuild);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = CoCreateInstance(CLSID_FilterGraph, 0, CLSCTX_INPROC_SERVER, IID_IFilterGraph, (void **) &pGraph);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        pBuild->SetFiltergraph(pGraph);
+
+        hr = CoCreateInstance(CLSID_SystemDeviceEnum, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pDevEnum));
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = pDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, &pEnum, 0);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+
+        cout << "Select Device :-" << endl;
+        while (pEnum->Next(1, &pMoniker, NULL) == S_OK) {
+            IPropertyBag *pPropBag;
+            hr = pMoniker->BindToStorage(0, 0, IID_PPV_ARGS(&pPropBag));
+            if (FAILED(hr)) {
+                pMoniker->Release();
+                continue;
+            }
+
+            VARIANT var;
+            VariantInit(&var);
+
+            hr = pPropBag->Read(L"FriendlyName", &var, 0);
+            if (SUCCEEDED(hr)) {
+                wcout << (wchar_t *) var.bstrVal << endl;
+                VariantClear(&var);
+            }
+
+            pPropBag->Release();
+            pMoniker->Release();
+        }
+        pEnum->Reset();
+        // cin >> DeviceNumber;
+        DeviceNumber = 1;
+        for (int i = 0; i < DeviceNumber; i++) {
+            hr = pEnum->Next(1, &pMoniker, NULL);
+            if (FAILED(hr)) {
+                /*Handle Error*/
+            }
+        }
+        hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void **) &pCap);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = pGraph->AddFilter(pCap, L"Capture Filter");
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = pGraph->QueryInterface(IID_IMediaControl, (void **) &pControl);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = pGraph->QueryInterface(IID_IVideoWindow, (void **) &pWindow);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+
+        // hr = pBuild->SetOutputFileName(&MEDIASUBTYPE_Avi, L"D:\\Repository\\RemoteControlViaEmail\\src\\camera\\sigma.avi", &pMux, NULL); // Remove this line if you only want a preview.
+        // if (FAILED(hr)) { /*Handle Error*/ } // Remove this line if you only want a preview.
+        // hr = pBuild->RenderStream(&PIN_CATEGORY_CAPTURE, &MEDIATYPE_Video, pCap, NULL, pMux); // Remove this line if you only want a preview.
+        // if (FAILED(hr)) { /*Handle Error*/ } // Remove this line if you only want a preview.
+
+        hr = pBuild->RenderStream(&PIN_CATEGORY_PREVIEW, &MEDIATYPE_Video, pCap, NULL, NULL);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+    }
     pControl->Run();
 
     pWindow->put_Caption((BSTR) L"Device Capture");
@@ -232,56 +305,153 @@ void StartWebcam_1() {
     CoUninitialize();
 }
 
+void WebcamController::WebcamThread() {
+    // Initialize DirectShow objects
+    ICaptureGraphBuilder2 *pBuild = nullptr;
+    IGraphBuilder *pGraph = nullptr;
+    IBaseFilter *pCap = nullptr;
+    IMediaControl *pControl = nullptr;
+    IVideoWindow *pWindow = nullptr;
 
-//HRESULT WebcamController::CaptureImage() {
-//    *ppSource = NULL;
-//
-//    UINT32 count = 0;
-//
-//    IMFAttributes *pConfig = NULL;
-//    IMFActivate **ppDevices = NULL;
-//
-//    // Create an attribute store to hold the search criteria.
-//    HRESULT hr = MFCreateAttributes(&pConfig, 1);
-//
-//    // Request video capture devices.
-//    if (SUCCEEDED(hr)) {
-//        hr = pConfig->SetGUID(
-//            MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
-//            MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID
-//        );
-//    }
-//
-//    // Enumerate the devices,
-//    if (SUCCEEDED(hr)) {
-//        hr = MFEnumDeviceSources(pConfig, &ppDevices, &count);
-//    }
-//
-//    // Create a media source for the first device in the list.
-//    if (SUCCEEDED(hr)) {
-//        if (count > 0) {
-//            hr = ppDevices[0]->ActivateObject(IID_PPV_ARGS(ppSource));
-//        } else {
-//            hr = MF_E_NOT_FOUND;
-//        }
-//    }
-//
-//    for (DWORD i = 0; i < count; i++) {
-//        ppDevices[i]->Release();
-//    }
-//    CoTaskMemFree(ppDevices);
-//    return hr;
-//}
+    // IBaseFilter *pMux; // Remove this line if you only want a preview.
 
-void WebcamController::StartWebcam() {
-    StartWebcam_1();
+    IMoniker *pMoniker;
+    IEnumMoniker *pEnum;
+    ICreateDevEnum *pDevEnum;
+
+
+    HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    if (SUCCEEDED(hr)) {
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+
+        hr = CoCreateInstance(CLSID_CaptureGraphBuilder2, NULL, CLSCTX_INPROC_SERVER, IID_ICaptureGraphBuilder2,
+                              (void **) &pBuild);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = CoCreateInstance(CLSID_FilterGraph, 0, CLSCTX_INPROC_SERVER, IID_IFilterGraph, (void **) &pGraph);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        pBuild->SetFiltergraph(pGraph);
+
+        hr = CoCreateInstance(CLSID_SystemDeviceEnum, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pDevEnum));
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = pDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, &pEnum, 0);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+
+        cout << "Select Device :-" << endl;
+        while (pEnum->Next(1, &pMoniker, NULL) == S_OK) {
+            IPropertyBag *pPropBag;
+            hr = pMoniker->BindToStorage(0, 0, IID_PPV_ARGS(&pPropBag));
+            if (FAILED(hr)) {
+                pMoniker->Release();
+                continue;
+            }
+
+            VARIANT var;
+            VariantInit(&var);
+
+            hr = pPropBag->Read(L"FriendlyName", &var, 0);
+            if (SUCCEEDED(hr)) {
+                wcout << (wchar_t *) var.bstrVal << endl;
+                VariantClear(&var);
+            }
+
+            pPropBag->Release();
+            pMoniker->Release();
+        }
+        pEnum->Reset();
+        // cin >> DeviceNumber;
+        int DeviceNumber = 1;
+        for (int i = 0; i < DeviceNumber; i++) {
+            hr = pEnum->Next(1, &pMoniker, NULL);
+            if (FAILED(hr)) {
+                /*Handle Error*/
+            }
+        }
+        hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void **) &pCap);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = pGraph->AddFilter(pCap, L"Capture Filter");
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = pGraph->QueryInterface(IID_IMediaControl, (void **) &pControl);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        hr = pGraph->QueryInterface(IID_IVideoWindow, (void **) &pWindow);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+
+        // hr = pBuild->SetOutputFileName(&MEDIASUBTYPE_Avi, L"D:\\Repository\\RemoteControlViaEmail\\src\\camera\\sigma.avi", &pMux, NULL); // Remove this line if you only want a preview.
+        // if (FAILED(hr)) { /*Handle Error*/ } // Remove this line if you only want a preview.
+        // hr = pBuild->RenderStream(&PIN_CATEGORY_CAPTURE, &MEDIATYPE_Video, pCap, NULL, pMux); // Remove this line if you only want a preview.
+        // if (FAILED(hr)) { /*Handle Error*/ } // Remove this line if you only want a preview.
+
+        hr = pBuild->RenderStream(&PIN_CATEGORY_PREVIEW, &MEDIATYPE_Video, pCap, NULL, NULL);
+        if (FAILED(hr)) {
+            /*Handle Error*/
+        }
+        if (SUCCEEDED(hr)) {
+            pControl->Run();
+            pWindow->put_Caption((BSTR) L"Device Capture");
+            std::unique_lock<std::mutex> lock(mtx);
+            while (!stopRequested) {
+                cv.wait_for(lock, std::chrono::milliseconds(100));
+                // Process messages if needed
+                MSG msg;
+                while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+                    TranslateMessage(&msg);
+                    DispatchMessage(&msg);
+                }
+            }
+            pControl->Stop();
+        }
+        pBuild->Release();
+        pGraph->Release();
+        pCap->Release();
+
+        // pMux->Release(); // Remove this line if you only want a preview.
+
+        pMoniker->Release();
+        pEnum->Release();
+        pDevEnum->Release();
+
+        pControl->Release();
+        pWindow->Release();
+    }
+    CoUninitialize();
+    isRunning = false;
 }
 
-HRESULT WebcamController::StopWebcam() {
-    if (pControl) {
-        return pControl->Stop();
+void WebcamController::StartWebcam() {
+    // StartWebcam_1();
+    if (!isRunning) {
+        isRunning = true;
+        stopRequested = false;
+        std::thread webcamThread(&WebcamController::WebcamThread, this);
+        webcamThread.detach();
     }
-    return E_FAIL;
+}
+
+void WebcamController::StopWebcam() {
+    // if (pControl) {
+    //     return pControl->Stop();
+    // }
+    if (isRunning) {
+        stopRequested = true;
+        cv.notify_one();
+    }
 }
 
 void WebcamController::CleanUp() {
